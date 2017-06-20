0be1335d551f2a8793fc2e4d4d9a05f31490a49e
albader94 <Albader@engr2-20-54-dhcp.int.colorado.edu>
2017-06-20 09:25:18 -0600

    c- code update

---
+++ lab1OnBoard/Debug/main.d
@@ -0,0 +1,17 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /Applications/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdint.h
+main.obj: /Applications/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdbool.h
+main.obj: /Users/Albader/ti/TivaWare_C_Series-2.1.3.156/inc/hw_memmap.h
+main.obj: /Users/Albader/ti/TivaWare_C_Series-2.1.3.156/driverlib/gpio.h
+main.obj: /Users/Albader/ti/TivaWare_C_Series-2.1.3.156/driverlib/sysctl.h
+main.obj: ../fib.asm
+
+../main.c: 
+/Applications/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdint.h: 
+/Applications/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdbool.h: 
+/Users/Albader/ti/TivaWare_C_Series-2.1.3.156/inc/hw_memmap.h: 
+/Users/Albader/ti/TivaWare_C_Series-2.1.3.156/driverlib/gpio.h: 
+/Users/Albader/ti/TivaWare_C_Series-2.1.3.156/driverlib/sysctl.h: 
+../fib.asm: 
