d62ab456d87727463f087bc3cf62d9e39589fd76
albader94 <Albader@Baders-MacBook-Pro.local>
2017-06-20 23:57:51 -0600

    Lab 1 Final
    - C code working up until fib(25)
    - Organized and commented C code
    - Fully functional Assembly code

--- lab1working/fib.asm
+++ lab1working/fib.asm
@@ -1,34 +1,35 @@
 
  .global fib
 
-
-
 fib:
 	push {lr}
+
 case_zero:
-	cmp r1, #0
-	beq case_one
+	cmp r0, #0
+	bne case_one
 	mov r0, #0
-	mov pc, lr
-	pop {pc}
+	pop{pc}
 
 case_one:
-	cmp r1, #1
-	beq fib_seq
+	cmp r0, #1
+	bne fib_seq
 	mov r0, #1
-	mov pc, lr
 	pop{pc}
 
 fib_seq:
+
 	push{r0}
 	sub r0, r0, #1
 	bl fib
 
 	mov r1, r0
 	pop{r0}
+
+	push{r1}
 	sub r0, r0, #2
 	bl fib
 
+	pop{r1}
 	add r0, r0, r1
-	pop {pc}
+	pop{pc}
 
